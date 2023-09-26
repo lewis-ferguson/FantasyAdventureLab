043924d62a690013bc593a2f9a8a96b2fd51531e
PaulJD91 <paul.dennehy@hotmail.com>
2023-09-26 14:06:28 +0100

    added sword test, hasWeaponType failing atm

--- src/test/java/SwordTest.java
+++ src/test/java/SwordTest.java
@@ -1,3 +1,30 @@
+import com.codebase.Sword;
+import com.codebase.WeaponType;
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+
 public class SwordTest {
-    
+
+    private Sword sword;
+
+    @Before
+    public void before() {
+        sword = new Sword(15, WeaponType.SWORD);
+    }
+
+    @Test
+    public void hasDamage() {
+        assertEquals(15, sword.getDamage());
+    }
+
+    @Test
+    public void hasWeaponType() {
+        assertEquals("Sword", sword.getWeaponType());
+    }
+
+    @Test
+    public void canAttack() {
+        assertEquals("Slash", sword.attack());
+    }
 }
