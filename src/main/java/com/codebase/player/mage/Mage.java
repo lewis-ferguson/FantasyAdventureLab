c15c7303e61bd37aa83220a87e3e671d5981c6e5
Lewis Ferguson <lewis.h.ferguson@btinternet.com>
2023-09-26 15:08:03 +0100

    added some imports to mage

--- src/main/java/com/codebase/player/mage/Mage.java
+++ src/main/java/com/codebase/player/mage/Mage.java
@@ -1,10 +1,13 @@
 package com.codebase.player.mage;
 
+import com.codebase.interfaces.IDefend;
+import com.codebase.interfaces.ISpell;
 import com.codebase.player.Player;
 
 public abstract class Mage extends Player implements IDefend, ISpell {
     public Mage(String name, int healthPoints) {
         super(name, healthPoints);
     }
+
 
 }
